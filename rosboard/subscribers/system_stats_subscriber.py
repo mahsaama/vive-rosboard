a8c0378e0b622975304d4c20e0859399b6578895
mahsaama <mahsa.ama1391@gmail.com>
2022-08-13 16:46:32 +0430

    changes in views added.

--- rosboard/subscribers/system_stats_subscriber.py
+++ rosboard/subscribers/system_stats_subscriber.py
@@ -9,12 +9,19 @@ import time
 import threading
 import traceback
 
+from rosboard.utils.running_services_utils import get_running_processes
+from rosboard.utils.wifi_utils import get_wifi_name, get_ip
+from rosboard.utils.usb_utils import get_usb
+
+
 def mean(list):
     return sum(list)/len(list)
 
+
 class SystemStatsSubscriber(object):
-    def __init__(self, callback):
+    def __init__(self, callback, net_name="wlan0"):
         self.callback = callback
+        self.net_name = net_name
         self.stop = False
         threading.Thread(target = self.start, daemon = True).start()
 
@@ -37,7 +44,7 @@ class SystemStatsSubscriber(object):
 
                 with p.oneshot():
                     sensors_temperatures = psutil.sensors_temperatures()
-                    cpu_percent = psutil.cpu_percent(percpu = True)
+                    cpu_percent = psutil.cpu_percent(percpu=True)
                     net_io_counters = psutil.net_io_counters()
                     virtual_memory = psutil.virtual_memory()
                     swap_memory = psutil.swap_memory()
@@ -45,6 +52,8 @@ class SystemStatsSubscriber(object):
 
                 status = {}
 
+                status["IP"] = get_ip(self.net_name)
+                status["SSID"] = get_wifi_name(self.net_name).replace('"', '')
                 status["cpu_percent"] = cpu_percent
 
                 if "coretemp" in sensors_temperatures:
@@ -52,20 +61,34 @@ class SystemStatsSubscriber(object):
                         lambda x:x.current,
                         sensors_temperatures["coretemp"]
                     )))
-                
+                if "thermal-fan-est" in sensors_temperatures:
+                    status["temp_coretemp"] = mean(list(map(
+                        lambda x:x.current,
+                        sensors_temperatures["thermal-fan-est"]
+                    )))
+
                 status["net_bytes_sent"] = net_io_counters.bytes_sent
                 status["net_bytes_recv"] = net_io_counters.bytes_recv
-                status["disk_usage_percent"] = disk_usage.percent
-                status["virtual_memory_percent"] = virtual_memory.percent
-                status["swap_memory_percent"] = swap_memory.percent
 
+                status["disk_usage_percent"] = disk_usage.percent
+                status["disk_usage_total"] = disk_usage.total / 1024.0 / 1024.0 / 1024.0
+                status["disk_usage_free"] = disk_usage.free / 1024.0 / 1024.0 / 1024.0
+
+                status["virtual_memory_percent"] = virtual_memory.percent
+                status["virtual_memory_total"] = virtual_memory.total / 1024.0 / 1024.0 / 1024.0
+                status["virtual_memory_available"] = virtual_memory.available / 1024.0 / 1024.0 / 1024.0
+
+                status["swap_memory_percent"] = swap_memory.percent
+                status["Running Services"] = get_running_processes()
+                status["USB"] = get_usb()
             except Exception as e:
                 traceback.print_exc()
 
             self.callback(status)
             time.sleep(3)
 
+
 if __name__ == "__main__":
     # Run test
-    SystemStatsSubscriber(lambda msg: print("Received msg: %s" % msg))
+    SystemStatsSubscriber(lambda msg: print("Received msg: %s" % msg), net_name="wlp3s0")
     time.sleep(100)
